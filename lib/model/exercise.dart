a0c5235ea2e5137c1b8d1290db5466c52355d0e3
RaGoose <daveyeinde@gmail.com>
2024-03-10 00:54:33 +0300

    exercise model

---
+++ lib/model/exercise.dart
@@ -0,0 +1,43 @@
+class Exercise {
+  final String id;
+  final String name;
+  final String force;
+  final String level;
+  final String mechanic;
+  final String equipment;
+  final List<String> primaryMuscles;
+  final List<String> secondaryMuscles;
+  final List<String> instructions;
+  final String category;
+  final List<String> images;
+
+  Exercise({
+    required this.id,
+    required this.name,
+    required this.force,
+    required this.level,
+    required this.mechanic,
+    required this.equipment,
+    required this.primaryMuscles,
+    required this.secondaryMuscles,
+    required this.instructions,
+    required this.category,
+    required this.images,
+  });
+
+  factory Exercise.fromJson(Map<String, dynamic> json) {
+    return Exercise(
+      id: json['id'],
+      name: json['name'],
+      force: json['force'],
+      level: json['level'],
+      mechanic: json['mechanic'],
+      equipment: json['equipment'],
+      primaryMuscles: List<String>.from(json['primaryMuscles']),
+      secondaryMuscles: List<String>.from(json['secondaryMuscles']),
+      instructions: List<String>.from(json['instructions']),
+      category: json['category'],
+      images: List<String>.from(json['images']),
+    );
+  }
+}

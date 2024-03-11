4f2243db0a629cf1a1481f2f8883bf2ecf40395d
RaGoose <daveyeinde@gmail.com>
2024-03-11 17:34:13 +0300

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pele_fit/view/home_page.dart';
 
+import 'view/detail_page.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -10,8 +12,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      routes: {
+        '/': (context) => const HomePage(),
+        '/detail': (context) => const DetailPage(),
+      },
     );
   }
 }
